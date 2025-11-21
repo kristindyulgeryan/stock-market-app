d8607248dde301125af0fbfe7a132b764dfc8a1b
Kristin <kristindyulgeryan@abv.bg>
2025-11-21 14:36:37 +0200

    Add daily news summary scheduled Inngest function

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -1,7 +1,8 @@
-import { sendVerificationEmailFn } from "better-auth/api";
 import { inngest } from "./client";
 import { PERSONALIZED_WELCOME_EMAIL_PROMPT } from "./prompts";
 import { sendWelcomeEmail } from "../nodemailer";
+
+import { getAllUsersForNewsEmail } from "../actions/user.actions";
 
 export const sendSignUpEmail = inngest.createFunction(
   { id: "sign-up-email" },
@@ -48,5 +49,17 @@ export const sendSignUpEmail = inngest.createFunction(
       success: true,
       message: "Welcome email sent successfully",
     };
+  }
+);
+
+export const sendDailyNewsSummary = inngest.createFunction(
+  { id: "daily-news-summery" },
+  [{ event: "app/send.daily.news" }, { cron: "0 12 * * *" }],
+  async ({ step }) => {
+    //  get all users for news delivery
+    const users = await step.run("get-all-users", getAllUsersForNewsEmail);
+
+    if (!users || users.length === 0)
+      return { success: false, message: "No users found for news email" };
   }
 );
